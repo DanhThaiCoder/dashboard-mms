3bd1f0512bf67670ef9beab020156f042dcc8439
DanhThaiCoder <157975851+DanhThaiCoder@users.noreply.github.com>
2026-06-13 16:59:02 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: '/dashboard-mms',
+  assetPrefix: '/dashboard-mms/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
