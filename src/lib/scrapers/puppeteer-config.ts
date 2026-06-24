f0259a0abebe7ca5b67352f4d8aaf33473f2e792
DanhThaiCoder <danhthaidoanbui@gmail.com>
2026-06-24 13:51:41 +0700

    new2

--- src/lib/scrapers/puppeteer-config.ts
+++ src/lib/scrapers/puppeteer-config.ts
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer-core';
-import chromium from '@sparticuz/chromium';
+import chromium from '@sparticuz/chromium-min';
 import { promises as fs } from 'fs';
 
 const getLocalExecutablePath = async (): Promise<string> => {
